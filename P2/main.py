fee3540fcec29786cb46141b299d48bf3f4ee6ea
Keyrat06 <raoulemil@gmail.com>
2017-09-25 22:25:46 -0400

    pushing batched

--- P2/main.py
+++ P2/main.py
@@ -1,4 +1,17 @@
 from loadFittingDataP2 import getData
+import numpy as np
 
 X, Y = getData(False)
+
+def expand(x, m):
+    x = np.array(x)
+    expanded = np.zeros([len(x), m+1])
+    for i in range(m+1):
+        expanded[:,i] = x**i
+    return expanded
+
+
+def SSE_poly(y, x, theta):
+    X = expand(x, len(theta))
+    error = y - np.matmul(X, theta)
 
