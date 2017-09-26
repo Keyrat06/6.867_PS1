1e74119253ac920af09ca497d7df89d6827015dd
Michael DeLaus <mikedelaus@dhcp-18-111-15-173.dyn.mit.edu>
2017-09-25 23:34:41 -0400

    max likelihood vector

--- P2/main.py
+++ P2/main.py
@@ -1,5 +1,8 @@
 from loadFittingDataP2 import getData
 import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+
 
 X, Y = getData(False)
 
@@ -15,3 +18,21 @@ def SSE_poly(y, x, theta):
     X = expand(x, len(theta))
     error = y - np.matmul(X, theta)
 
+
+def maxLikelihoodVector(x,y,m):
+    x_panded = expand(x,m)
+    x_panded = np.matrix(x_panded)
+    y = np.matrix(y)
+    w = np.linalg.inv(x_panded.T*x_panded)*x_panded.T*y.T
+    return w
+
+maxVec = maxLikelihoodVector(X,Y,3)
+X_test = np.linspace(0,1,100)
+sol = np.matmul(expand(X_test,3),maxVec)
+plt.plot(X_test,sol)
+
+plt.plot(X,Y,'o')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
